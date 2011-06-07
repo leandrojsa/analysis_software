3c534389e4fbc5c8b60b5818fb2e7265b220b8d6
Leandro Andrade <leojsandrade@gmail.com>
2011-06-07 12:23:32 -0300

    contando numero de commits por arquivo

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -6,15 +6,31 @@ class AnalysisController < ApplicationController
     if params[:url].nil? and params[:extension].nil?
       flash[:notice] = "Preencha todos os campos."
     else
-      #%x[svn log -v --xml --incremental  params[:url] >  exit.xml]
       #system("svn log -v --xml --incremental " + params[:url] + " > exit.xml" )
-      #@xml = REXML::Document.new File.open("exit.xml")
       @doc = Nokogiri::XML(File.open("exit.xml"))
-     # f = File.open("exit.xml")
-     # doc = Nokogiri::XML(f)
 
-      #f.close
+      @num_revisions = 0
+      @hash_files = Hash.new
+      @doc.css("logentry").each{ |logentry|
+        @num_revisions += 1
+        logentry.css("paths").each{|paths|
+          paths.css("path").each{|path|
+            file_path = String.new
+            if !path["copyfrom-path"].nil?
+              file_path = path["copyfrom-path"]
+            end
+            file_path += path.text
+            if @hash_files[file_path].nil?
+              @hash_files[file_path] = 1
+            else
+              @hash_files[file_path] += 1
+            end
 
+          }
+
+        }
+
+      }
     end
   end
 
